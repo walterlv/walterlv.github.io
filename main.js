3048b357e7890cb2fe5a91aa1684297654294d8f
吕毅 <walterlv@outlook.com>
2016-06-14 19:16:29 +0800

    Replace Hello World to double line Hello World

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var button = document.getElementById("searchButton");
+    button.onclick = searchButtonOnClick;
+}
+
+function searchButtonOnClick() {
+    var textInput = document.getElementById("searchTextInput");
+    var text = textInput.value;
+    if (text != "") {
+        var resultList = document.getElementById("resultList");
+        alert("Click");
+    }
+}
