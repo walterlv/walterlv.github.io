0b4f0606d33f4c3b71d61db554ce58f165f73498
walterlv <xblero@gmail.com>
2019-09-27 09:52:16 +0800

    Use local image to existed url replacer.

--- build/mdcsdn.ps1
+++ build/mdcsdn.ps1
@@ -1,1 +1,1 @@
-dotnet build\mdmeta\mdmeta.dll wcsdn _posts/此处填充文件名.md -b . -s https://blog.walterlv.com -l build/csdn-license.md
+dotnet build\mdmeta\mdmeta.dll wcsdn _posts/此处填充文件名.md -b . -i https://blog.walterlv.com -s https://blog.walterlv.com -l build/csdn-license.md
