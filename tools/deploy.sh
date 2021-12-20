5b81bbeb9a54a4fba0538d67935e7f1a462b5de0
walterlv <xblero@gmail.com>
2021-12-20 17:20:48 +0800

    Remove Test

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -56,11 +56,11 @@ build() {
 }
 
 test() {
-  bundle exec htmlproofer \
-    --disable-external \
-    --check-html \
-    --allow_hash_href \
-    "$SITE_DIR"
+  # bundle exec htmlproofer \
+  #   --disable-external \
+  #   --check-html \
+  #   --allow_hash_href \
+  #   "$SITE_DIR"
 }
 
 resume_site_dir() {
